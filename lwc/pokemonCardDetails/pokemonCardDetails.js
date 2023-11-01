f8fb91db5d3440354ae3b6cd1b19e2fe9205d700
Jyothsna230 <143384604+Jyothsna230@users.noreply.github.com>
2023-11-01 21:28:14 +0530

    Commit Deploy

--- lwc/pokemonCardDetails/pokemonCardDetails.js
+++ lwc/pokemonCardDetails/pokemonCardDetails.js
@@ -5,7 +5,7 @@ export default class PokemonCardDetails extends LightningElement {
 
     Pokemons;
     error;
-    serachvalue ='';
+    serachvalue ='Pokemon';
     isSearchNotAvailable = false;
     
     connectedCallback(){
